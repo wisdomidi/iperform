3f44bcb60316e431530df8e67ebd4154f484018a
Wisdom Idi <oweezyidi@gmail.com>
2020-04-16 06:16:10 +0100

    .

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL - DONE
-SQLALCHEMY_DATABASE_URI = "postgres://wisdomidi:Sososoweto2010@localhost:5432/fyyur"
-#SQLALCHEMY_DATABASE_URI = "postgres://xqpumnqcsjjpaa:4a81ee23944c334175c4809089519af0a029f6448079f15d66d4be58d433df75@ec2-3-211-48-92.compute-1.amazonaws.com:5432/ddod2t9f2hp648"
+#SQLALCHEMY_DATABASE_URI = "postgres://wisdomidi:Sososoweto2010@localhost:5432/fyyur"
+SQLALCHEMY_DATABASE_URI = "postgres://xqpumnqcsjjpaa:4a81ee23944c334175c4809089519af0a029f6448079f15d66d4be58d433df75@ec2-3-211-48-92.compute-1.amazonaws.com:5432/ddod2t9f2hp648"
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
